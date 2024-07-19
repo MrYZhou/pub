9b08b7f3498b500d8e40822f2e3c92a28f8d0a63
lg <1762861715@qq.com>
2024-07-19 12:06:07 +0800

    1

--- util/system.go
+++ util/system.go
@@ -1,15 +1,12 @@
 package util
 
 import (
-	. "log/slog"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 var app *fiber.App
-
-var staticDir = "./static"
 
 func App() *fiber.App {
 	if app == nil {
@@ -19,19 +16,10 @@ func App() *fiber.App {
 		// 注册自定义中间件以转换上下文
 		app.Use(CtxMiddleware)
 		// 静态文件服务
-		// 打包exe文件
-		if os.Getenv("GO_BUILD_TAGS") == "prod" {
-			homeDir, _ := os.UserHomeDir()
-			staticDir = homeDir + "/pub"
-		}
-
-		staticDirEnv := os.Getenv("staticDir")
-		if staticDirEnv != "" {
-			staticDir = staticDirEnv
-		}
-
-		Info("映射目录" + staticDir)
-		app.Static("/", staticDir)
+		homeDir, _ := os.UserHomeDir()
+		currentDir, _ := os.Getwd()
+		app.Static("/file", currentDir+"/static")
+		app.Static("/", homeDir+"/pub")
 	}
 
 	return app
