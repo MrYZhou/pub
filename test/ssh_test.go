8d25d7bd427fb54dcca38870de101c755dd45386
larry <1762861715@qq.com>
2024-07-25 00:59:32 +0800

    ssh连接

--- test/ssh_test.go
+++ test/ssh_test.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"io"
+	"log"
 	. "log/slog"
+	"net"
+	"os"
 	"testing"
 
 	. "github.com/MrYZhou/outil/ssh"
+	"golang.org/x/crypto/ssh"
 )
 
 // 测试连通性
@@ -18,12 +23,58 @@ func TestConnect(t *testing.T) {
 	}
 }
 func TestConnect2(t *testing.T) {
-	// 通过密钥
-	c, err := Server("47.120.11.192", "root", "!123qweA")
+
+	// 读取私钥
+	privateBytes, err := os.ReadFile("d:/pub/larry.pem")
+	if err != nil {
+		log.Fatalf("Failed to read private key file: %v", err)
+	}
+
+	// 解析PEM格式的私钥
+	privateKey, err := ssh.ParsePrivateKey(privateBytes)
+	if err != nil {
+		log.Fatalf("Failed to parse private key: %v", err)
+	}
+
+	// 创建SSH认证方法
+	authMethod := ssh.PublicKeys(privateKey)
+
+	// 连接到SSH服务器
+	config := &ssh.ClientConfig{
+		User: "root",
+		Auth: []ssh.AuthMethod{authMethod},
+		HostKeyCallback: ssh.HostKeyCallback(func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+			// 检查主机密钥，这里使用一个简单的回调函数忽略检查
+			return nil
+		}),
+	}
+
+	// 连接SSH服务器
+	conn, err := ssh.Dial("tcp", "47.110/*  */.11.197:22", config)
+	if err != nil {
+		log.Fatalf("Failed to dial: %v", err)
+	}
+	defer conn.Close()
+
+	// 打开一个新的会话
+	session, err := conn.NewSession()
+	if err != nil {
+		log.Fatalf("Failed to create session: %v", err)
+	}
+	defer session.Close()
+
 	if err != nil {
 		Info("连接失败")
 	} else {
-		Info(c.Host)
+		Info("连接成功")
+		r, err := session.StdoutPipe()
+		if err != nil {
+			os.Exit(1001)
+		}
+		go io.Copy(os.Stdout, r)
+
+		buf, err := session.CombinedOutput("pwd")
+		Info(string(buf))
 	}
 }
 
